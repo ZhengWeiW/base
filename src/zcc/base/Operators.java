fd66ba8d396de143e3162b0907da3d36a0972c0e
郑韦伟 <zhengww1@akulaku.com>
2024-07-19 15:42:11 +0800

    添加注释

---
+++ src/zcc/base/Operators.java
@@ -0,0 +1,41 @@
+package zcc.base;
+
+/**
+ * @description:
+ * @author: zcc
+ * @createDate: 2024/7/19
+ * @version: 1.0
+ */
+public class Operators {
+    public static void main(String[] args) {
+        int i = -1;
+        System.out.println("初始数据：" + i);
+        System.out.println("初始数据对应的二进制字符串：" + Integer.toBinaryString(i));
+        i <<= 10;
+        System.out.println("左移 10 位后的数据 " + i);
+        System.out.println("左移 10 位后的数据对应的二进制字符 " + Integer.toBinaryString(i));
+
+        int b = 1;
+        System.out.println("初始数据：" + b);
+        System.out.println("初始数据对应的二进制字符串：" + Integer.toBinaryString(b));
+        b <<= 10;
+        System.out.println("左移 10 位后的数据 " + b);
+        System.out.println("左移 10 位后的数据对应的二进制字符 " + Integer.toBinaryString(b));
+
+        int c = 1;
+        System.out.println("初始数据：" + c);
+        System.out.println("初始数据对应的二进制字符串：" + Integer.toBinaryString(c));
+        c >>= 10;
+        System.out.println("左移 10 位后的数据 " + c);
+        System.out.println("左移 10 位后的数据对应的二进制字符 " + Integer.toBinaryString(c));
+
+
+        int d = -1;
+        System.out.println("初始数据：" + d);
+        System.out.println("初始数据对应的二进制字符串：" + Integer.toBinaryString(d));
+        d >>= 10;
+        System.out.println("左移 10 位后的数据 " + d);
+        System.out.println("左移 10 位后的数据对应的二进制字符 " + Integer.toBinaryString(d));
+
+    }
+}
